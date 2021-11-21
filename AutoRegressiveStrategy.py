4213fc3370712cba0681163763c24da265f6e27d
saganaki-xyz <saganaki@saganaki.xyz>
2021-11-20 22:04:56 -0500

    update ar data cleaning

--- AutoRegressiveStrategy.py
+++ AutoRegressiveStrategy.py
@@ -36,15 +36,16 @@ class AutoRegressiveStrategy:
     
     def clean_data_for_garch(self,data_in):
             percentile_remove            = .01
+            z_score_cutoff               = 3
             data_filled                  = ActiveStrategyFramework.fill_time(data_in)
             top_ptile                    = np.nanquantile(data_filled.quotePrice,1-percentile_remove)
             bot_ptile                    = np.nanquantile(data_filled.quotePrice,percentile_remove)
             data_filled                  = data_filled[(data_filled.quotePrice < top_ptile) & (data_filled.quotePrice  > bot_ptile) ]
             data_filled['price_return']  = data_filled['quotePrice'].pct_change()
-            top_ptile                    = np.nanquantile(data_filled.price_return,1-percentile_remove)
-            bot_ptile                    = np.nanquantile(data_filled.price_return,percentile_remove)
-            data_filled                  = data_filled[(data_filled.price_return < top_ptile) & (data_filled.price_return  > bot_ptile) ]
-            data_filled['price_return']  = data_filled['quotePrice'].pct_change()
+            data_filled['price_return']  = data_filled.quotePrice.pct_change()
+            data_filled['z_scores']      = np.abs(scipy.stats.zscore(data_filled['price_return']))
+            data_filled                  = data_filled.drop(data_filled[data_filled.z_scores > z_score_cutoff].index)
+            data_filled                  = data_filled['quotePrice']
             return data_filled
         
         
